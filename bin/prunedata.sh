e0f170a52afd8363e1a6a80d60ca263adb7c0d3b
Lance Berc <lanceb@gmail.com>
2022-11-28 09:06:46 -0800

    Remove empty data directories

--- bin/prunedata.sh
+++ bin/prunedata.sh
@@ -3,5 +3,8 @@
 sh /home/stfyc/bin/prunemp4.sh
 
 find /home/stfyc/www/html/data/GOES -ctime +8 | xargs rm
+find /home/stfyc/www/html/data/GOES -ctime +8 -type d -empty | xargs rmdir
 find /home/stfyc/www/html/data/overlay -ctime +6 | xargs rm
+find /home/stfyc/www/html/data/overlay -ctime +6 -type d -empty | xargs rmdir
 find /home/stfyc/www/html/data/SFBOFS -ctime +1 | xargs rm
+find /home/stfyc/www/html/data/SFBOFS -ctime +1 -type d -empty | xargs rmdir
