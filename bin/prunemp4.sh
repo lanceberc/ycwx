70952afbd4dd97c167a6f77daadcf18551cf1f88
Lance Berc <lanceb@gmail.com>
2023-03-05 15:49:25 -0800

    Improve removing all but the last 4 entries

--- bin/prunemp4.sh
+++ bin/prunemp4.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 # remember that "latest.mp4" is out there too
-for d in WestCoast BayDelta Pacific
+for d in WestCoast BayDelta Pacific EastPacificGLM WestCoastGLM
 do
-    ls /home/stfyc/www/html/data/overlay/${d}/${d}*.mp4 | head -n -4 | xargs rm
+    ls -1 /home/stfyc/www/html/data/overlay/${d}/${d}*.mp4 | head -n -4 | xargs rm
 done
