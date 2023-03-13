073c151104e99d90734d39cdd79f3e69822dd2fb
Lance Berc <lanceb@gmail.com>
2023-03-13 12:56:24 -0700

    Check if directory exists before trying to remove mp4s

--- bin/prunemp4.sh
+++ bin/prunemp4.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
 # remember that "latest.mp4" is out there too
-for d in WestCoast BayDelta Pacific EastPacificGLM WestCoastGLM
+for d in WestCoast BayDelta BayDelta500m Pacific EastPacificGLM WestCoastGLM
 do
-    ls -1 /home/stfyc/www/html/data/overlay/${d}/${d}*.mp4 | head -n -4 | xargs rm
+    if [ -d /home/stfyc/www/html/data/overlay/${d} ]
+    then
+	ls -1 /home/stfyc/www/html/data/overlay/${d}/${d}*.mp4 | head -n -4 | xargs rm
+    fi
 done
