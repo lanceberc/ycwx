32a9267423781628bf2cae7e3ee85bf4dc0edf36
Lance Berc <lanceb@gmail.com>
2023-04-24 04:36:17 -0700

    Improve usage by changing to new tiled NOAA sources, clean up cache_defeat config, and other items Add configs for SST, Wind, and Waves

--- www/html/gisConfig.js
+++ www/html/gisConfig.js
@@ -3,7 +3,7 @@
 const crossorigin='';
 //const crossorigin = "anonymous";
 
-// GIS adds a layer created in Javascript for the observation text boxes
+// wx.html adds a layer created in Javascript for the observation text boxes
 const NOAAObservationsConfig = {
     container: "NOAAObservations",
     label: "NOAAObservationsLabel",
@@ -17,6 +17,25 @@ const NOAAObservationsConfig = {
 	    type: 'XYZ',
 	    url: 'https://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer',
 	},
+	{
+	    type: 'WMS',
+	    url: 'https://nowcoast.noaa.gov/geoserver/alerts/wms',
+	    cache_defeat: 10 * 60,
+	    params: {
+		'LAYERS': 'watches_warnings_advisories',
+		'FORMAT': 'image/png',
+	    },
+	    cssClass: "ol-semi-transparent",
+	},
+	{
+	    type: 'WMS',
+	    url: 'https://opengeo.ncep.noaa.gov/geoserver/conus/conus_bref_qcd/ows?SERVICE=WMS&',
+	    cache_defeat: 5 * 60,
+	    params: {
+		'LAYERS': 'conus_bref_qcd',
+		'FORMAT': 'image/png',
+	    },
+	},
     ],
 }
 
@@ -28,13 +47,17 @@ const nationalRadarConfig = {
     updateFrequency: 1 * 60 * 1000, // GIS class hardcodes updates every 10 minutes but we'll poll once a minute
     layers: [
 	{
-	    type: 'ArcGISRest',
+	    type: 'XYZ',
 	    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer',
 	},
 	{
-	    type: 'ArcGISRest',
-	    url: 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer',
-	    updateFrequency: 5 * 60 * 1000,
+	    type: 'WMS',
+	    url: 'https://opengeo.ncep.noaa.gov/geoserver/conus/conus_bref_qcd/ows?SERVICE=WMS&',
+	    cache_defeat: 5 * 60,
+	    params: {
+		'LAYERS': 'conus_bref_qcd',
+		'FORMAT': 'image/png',
+	    },
 	},
     ],
 }
@@ -47,69 +70,44 @@ const nationalRadar2Config = {
     updateFrequency: 1 * 60 * 1000, // GIS class hardcodes updates every 10 minutes but we'll poll once a minute
     layers: [
 	{
-	    type: "ArcGISRest",
+	    type: "XYZ",
 	    url: 'https://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer',
-	    params: {
-		'CRS': '3857',
-	    },
 	},
 	{
 	    type: "ArcGISRest",
 	    url: 'https://carto.nationalmap.gov/arcgis/rest/services/govunits/MapServer',
 	    params: {
 		'layers': 'show:21',
-		'CRS': '3857',
 	    },
 	},
 	{
 	    type: 'ArcGISRest',
-	    url: 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/WWA/watch_warn_adv/MapServer',
-	    updateFrequency: 30 * 60 * 1000,
+	    url: 'https://mapservices.weather.noaa.gov/static/rest/services/nws_reference_maps/nws_reference_map/MapServer',
 	    params: {
-		'CRS': '3857',
+		//'LAYERS': 'show:3,5,6',
+		'LAYERS': 'show:5', // Coastal and offshore (but not high seas) warning areas
 	    },
 	    cssClass: "ol-semi-transparent",
 	},
-	{
-	    type: 'ArcGISRest',
-	    url: 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer',
-	    params: {
-		'CRS': '3857',
-	    },
-	    updateFrequency: 5 * 60 * 1000,
-	},
-	/*
-	{
-	    type: 'ArcGISRest',
-	    url: 'https://ibasemaps-api.arcgis.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer',
-	    params: {
-		'TOKEN': esri_apikey,
-	    }
-	},
-	*/
-	/*
 	{
 	    type: 'WMS',
 	    url: 'https://nowcoast.noaa.gov/geoserver/alerts/wms',
-	    updateFrequency: 5 * 60 * 1000,
 	    params: {
 		'LAYERS': 'watches_warnings_advisories',
 		'FORMAT': 'image/png',
-		//'CRS': 'EPSG:3857',
 	    },
+	    cache_defeat: 5 * 60,
 	    cssClass: "ol-semi-transparent",
 	},
 	{
 	    type: 'WMS',
-	    url: 'https://nowcoast.noaa.gov/geoserver/weather_radar/wms',
-	    updateFrequency: 5 * 60 * 1000,
+	    url: 'https://opengeo.ncep.noaa.gov/geoserver/conus/conus_bref_qcd/ows?SERVICE=WMS&',
 	    params: {
-		'LAYERS': 'base_reflectivity_mosaic',
+		'LAYERS': 'conus_bref_qcd',
 		'FORMAT': 'image/png',
-		'CRS': '3857',
 	    },
+	    cache_defeat: 5 * 60,
 	},
-	*/
     ],
 }
 
@@ -122,57 +120,26 @@ const localRadarConfig = {
     layers: [
 	{
 	    type: 'XYZ',
-	    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer',
-	    updateFrequency: 15 * 60 * 1000,
+	    url: 'https://server.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer',
 	},
 	{
-	    type: "WMS",
-	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_longduration_hazards_time/MapServer/WmsServer',
+	    type: 'WMS',
+	    url: 'https://nowcoast.noaa.gov/geoserver/alerts/wms',
+	    cache_defeat: 5 * 60,
 	    params: {
-		//'LAYERS': '1,4,10,13,17,20,24,27,31,34,38,41,6,7,1,2',
-		'LAYERS': '1,2,4,5,7,8,10,11,13,14,17,18,20,21,24,25,27,28,31,32,34,35,38,39,41,42',
+		'LAYERS': 'watches_warnings_advisories',
 		'FORMAT': 'image/png',
-		crossOrigin: crossorigin,
 	    },
-	    service: 'wwa_meteoceanhydro_longduration_hazards_time',
-	    timeStopLayers: '0,2,3,5,6,7,9,10,12,13,16,19,21,23,24,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42',
 	    cssClass: "ol-semi-transparent",
-	    updateFrequency: 10 * 60 * 1000,
 	},
 	{
-	    type: "WMS",
-	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WmsServer',
+	    type: 'WMS',
+	    url: 'https://opengeo.ncep.noaa.gov/geoserver/conus/conus_bref_qcd/ows?SERVICE=WMS&',
+	    cache_defeat: 5 * 60,
 	    params: {
-		'LAYERS': '1',
+		'LAYERS': 'conus_bref_qcd',
 		'FORMAT': 'image/png',
-		// crossOrigin: crossorigin,
 	    },
-	    service: 'radar_meteo_imagery_nexrad_time',
-	    updateFrequency: 2 * 60 * 1000,
-	},
-	{
-	    type: "WMS",
-	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_shortduration_hazards_watches_time/MapServer/WmsServer',
-	    params: {
-		'LAYERS': '0,1,2',
-		'FORMAT': 'image/png',
-		crossOrigin: crossorigin,
-	    },
-	    service: 'wwa_meteoceanhydro_shortduration_hazards_watches_time',
-	    cssClass: "ol-semi-transparent",
-	    updateFrequency: 10 * 60 * 1000,
-	},
-	{
-	    type: "WMS",
-	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_shortduration_hazards_warnings_time/MapServer/WmsServer',
-	    params: {
-		'LAYERS': '0,1,2,3,4,5,6,7',
-		'FORMAT': 'image/png',
-		crossOrigin: crossorigin,
-	    },
-	    service: 'wwa_meteoceanhydro_shortduration_hazards_warnings_time',
-	    cssClass: "ol-semi-transparent",
-	    updateFrequency: 10 * 60 * 1000,
 	},
     ],
 }
@@ -193,7 +160,7 @@ const cloudTopsConfig = {
 	    type: "XYZ",
 	    url: 'https://earthlive.maptiles.arcgis.com/arcgis/rest/services/GOES/GOES31C/MapServer',
 	    attributions: "",
-	    updateFrequency: 5 * 60 * 1000,
+	    cache_defeat: 5 * 60,
 	},
 	{
 	    type: "ArcGISRest",
@@ -201,6 +168,132 @@ const cloudTopsConfig = {
 	    attributions: "",
 	    params: {
 		'layers': 'show:21',
+	    },
+	},
+    ],
+}
+
+const SSTConfig = {
+    container: "SST",
+    label: "SSTLabel",
+    center: [-143, 34.5], // Mid-Pacific
+    zoom: 5,
+    //center: [-140, 10], // Mid-Pacific
+    //zoom: 3,
+    updateFrequency: 60 * 60 * 1000,
+    layers: [
+	{
+	    type: 'XYZ',
+	    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer',
+	},
+	{
+	    type: 'WMS',
+	    url: 'https://coastwatch.pfeg.noaa.gov/erddap/wms/jplMURSST41/request?',
+	    params: {
+		'LAYERS': 'jplMURSST41:analysed_sst',
+		'FORMAT': 'image/png',
+	    },
+	    projection: 'EPSG:4326',
+	    cache_defeat: 6 * 60 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://geonode.state.gov/geoserver/ows',
+	    params: {
+		'layers': 'geonode:LSIB', // Large Scale International Boundries
+	    },
+	},
+    ],
+}
+
+const SSTanomalyConfig = {
+    container: "SSTanomaly",
+    label: "SSTanomalyLabel",
+    center: [-143, 37.5], // Mid-Pacific
+    zoom: 5,
+    updateFrequency: 60 * 60 * 1000,
+    layers: [
+	{
+	    type: 'XYZ',
+	    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer',
+	},
+	{
+	    type: 'WMS',
+	    url: 'https://coastwatch.pfeg.noaa.gov/erddap/wms/jplMURSST41anom1day/request?',
+	    params: {
+		'LAYERS': 'jplMURSST41anom1day:sstAnom',
+		'FORMAT': 'image/png',
+	    },
+	    projection: 'EPSG:4326',
+	    cache_defeat: 6 * 60 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://geonode.state.gov/geoserver/ows',
+	    params: {
+		'layers': 'geonode:LSIB', // Large Scale International Boundries
+	    },
+	},
+    ],
+}
+
+const GlobalWindConfig = {
+    container: "GlobalWind",
+    label: "GlobalWindLabel",
+    center: [-143, 34.5], // Mid-Pacific
+    zoom: 5,
+    updateFrequency: 60 * 60 * 1000,
+    layers: [
+	{
+	    type: 'XYZ',
+	    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer',
+	},
+	{
+	    type: 'WMS',
+	    url: 'https://coastwatch.pfeg.noaa.gov/erddap/wms/jplMURSST41/request?',
+	    params: {
+		'LAYERS': 'erdQCwindproducts3day:wind_speed',
+		'FORMAT': 'image/png',
+	    },
+	    projection: 'EPSG:4326',
+	    cache_defeat: 6 * 60 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://geonode.state.gov/geoserver/ows',
+	    params: {
+		'layers': 'geonode:LSIB', // Large Scale International Boundries
+	    },
+	},
+    ],
+}
+
+const GlobalWaveConfig = {
+    container: "GlobalWave",
+    label: "GlobalWaveLabel",
+    center: [-143, 34.5], // Mid-Pacific
+    zoom: 5,
+    updateFrequency: 60 * 60 * 1000,
+    layers: [
+	{
+	    type: 'XYZ',
+	    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer',
+	},
+	{
+	    type: 'WMS',
+	    url: 'https://coastwatch.pfeg.noaa.gov/erddap/wms/NWW3_Global_Best/request?',
+	    params: {
+		'LAYERS': 'ww3_global_lon180:Thgt',
+		'FORMAT': 'image/png',
+	    },
+	    projection: 'EPSG:4326',
+	    cache_defeat: 6 * 60 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://geonode.state.gov/geoserver/ows',
+	    params: {
+		'layers': 'geonode:LSIB', // Large Scale International Boundries
 	    },
 	},
     ],
@@ -229,45 +322,6 @@ const currentMapConfig = {
 		'FORMAT': 'image/png',
 	    },
 	    timeSource: true,
-	},
-    ],
-}
-
-// https://nowcoast.noaa.gov/help/#!section=wms-layer-ids
-
-const oldNationalRadarConfig = {
-    container: "nationalRadar",
-    label: "nationalRadarLabel",
-    center: [-97.95, 38.91], // Longitude, latitude - Middle of Kansas,
-    //center: [-122.43, 37.82], // San Francisco
-    zoom: 4,
-    //timeSpan: 1 * 60 * 60 * 1000,
-    //frameInterval: 500,  // msec between frames
-    //lastFrameHoldInterval: 5000,
-    //firstTimeStop: 0,
-    //lastTimeStop: 0,
-    //timeStopLayers: '3',
-    //timeStopService: "radar_meteo_imagery_nexrad_time",
-    //timeStopUpdateInterval: 5 * 60 * 1000,
-    updateFrequency: 1 * 60 * 1000, // GIS class hardcodes updates every 10 minutes but we'll poll once a minute
-    layers: [
-	{
-	    type: 'XYZ',
-	    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-	    attributions: 'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' + 'rest/services/World_Topo_Map/MapServer">ArcGIS</a>',
-	    crossOrigin: "anonymous",
-	},
-	{
-	    type: "XYZ",
-	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/tile/{z}/{y}/{x}',
-	    attributions: 'NOAA NEXRAD',
-	    params: {
-		'LAYERS': '1',
-		'FORMAT': 'image/png',
-	    },
-	    //crossOrigin: "anonymous",
-	    timeSource: true,
-	    update: true,
 	},
     ],
 }
@@ -326,6 +380,108 @@ nowCoastElements = [
     */
 ];
 
+// https://nowcoast.noaa.gov/help/#!section=wms-layer-ids
+const oldNationalRadarConfig = {
+    container: "nationalRadar",
+    label: "nationalRadarLabel",
+    center: [-97.95, 38.91], // Longitude, latitude - Middle of Kansas,
+    //center: [-122.43, 37.82], // San Francisco
+    zoom: 4,
+    //timeSpan: 1 * 60 * 60 * 1000,
+    //frameInterval: 500,  // msec between frames
+    //lastFrameHoldInterval: 5000,
+    //firstTimeStop: 0,
+    //lastTimeStop: 0,
+    //timeStopLayers: '3',
+    //timeStopService: "radar_meteo_imagery_nexrad_time",
+    //timeStopUpdateInterval: 5 * 60 * 1000,
+    updateFrequency: 1 * 60 * 1000, // GIS class hardcodes updates every 10 minutes but we'll poll once a minute
+    layers: [
+	{
+	    type: 'XYZ',
+	    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+	    attributions: 'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' + 'rest/services/World_Topo_Map/MapServer">ArcGIS</a>',
+	    crossOrigin: "anonymous",
+	},
+	{
+	    type: "XYZ",
+	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/tile/{z}/{y}/{x}',
+	    attributions: 'NOAA NEXRAD',
+	    params: {
+		'LAYERS': '1',
+		'FORMAT': 'image/png',
+	    },
+	    //crossOrigin: "anonymous",
+	    timeSource: true,
+	    update: true,
+	},
+    ],
+}
+
+const oldLocalRadarConfig = {
+    container: "localRadar",
+    label: "localRadarLabel",
+    center: [-122.43, 37.82], // San Francisco
+    zoom: 7,
+    updateFrequency: 1 * 60 * 1000, // GIS class hardcodes updates every 10 minutes but we'll poll once a minute
+    layers: [
+	{
+	    type: 'XYZ',
+	    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer',
+	    cache_defeat: 15 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_longduration_hazards_time/MapServer/WmsServer',
+	    params: {
+		//'LAYERS': '1,4,10,13,17,20,24,27,31,34,38,41,6,7,1,2',
+		'LAYERS': '1,2,4,5,7,8,10,11,13,14,17,18,20,21,24,25,27,28,31,32,34,35,38,39,41,42',
+		'FORMAT': 'image/png',
+		crossOrigin: crossorigin,
+	    },
+	    service: 'wwa_meteoceanhydro_longduration_hazards_time',
+	    timeStopLayers: '0,2,3,5,6,7,9,10,12,13,16,19,21,23,24,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42',
+	    cssClass: "ol-semi-transparent",
+	    cache_defeat: 10 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WmsServer',
+	    params: {
+		'LAYERS': '1',
+		'FORMAT': 'image/png',
+		// crossOrigin: crossorigin,
+	    },
+	    service: 'radar_meteo_imagery_nexrad_time',
+	    cache_defeat: 2 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_shortduration_hazards_watches_time/MapServer/WmsServer',
+	    params: {
+		'LAYERS': '0,1,2',
+		'FORMAT': 'image/png',
+		crossOrigin: crossorigin,
+	    },
+	    service: 'wwa_meteoceanhydro_shortduration_hazards_watches_time',
+	    cssClass: "ol-semi-transparent",
+	    cache_defeat: 10 * 60,
+	},
+	{
+	    type: "WMS",
+	    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/wwa_meteoceanhydro_shortduration_hazards_warnings_time/MapServer/WmsServer',
+	    params: {
+		'LAYERS': '0,1,2,3,4,5,6,7',
+		'FORMAT': 'image/png',
+		crossOrigin: crossorigin,
+	    },
+	    service: 'wwa_meteoceanhydro_shortduration_hazards_warnings_time',
+	    cssClass: "ol-semi-transparent",
+	    cache_defeat: 10 * 60,
+	},
+    ],
+}
+
 // A bunch of layers that either didn't look good, didn't work well, or couldn't get to work at all.
 const otherRandomLayersConfig = {
     container: "randomLayers",
@@ -334,6 +490,14 @@ const otherRandomLayersConfig = {
     zoom: 5,
     updateFrequency: 1 * 60 * 1000, // GIS class hardcodes updates every 10 minutes but we'll poll once a minute
     layers: [
+	{
+	    // A fine basemap that needs an ESRI key
+	    type: 'XYZ',
+	    url: 'https://ibasemaps-api.arcgis.com/arcgis/rest/services/World_Imagery/MapServer',
+	    params: {
+		'token': esri_apikey,
+	    },
+	},
 	{
 	    // This would be a nice layer if oceans were transparent
 	    type: 'ArcGISRest',
@@ -351,7 +515,7 @@ const otherRandomLayersConfig = {
 		'FORMAT': 'image/png',
 	    },
 	    attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-	    //updateFrequency: 60 * 60 * 1000,
+	    //cache_defeat: 60 * 60,
 	},
 	{
 	    // Couldn't make this one work - I think we aren't passing the format parameter
@@ -398,7 +562,7 @@ const otherRandomLayersConfig = {
 	    type: 'ArcGISRest',
 	    url: 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/WWA/watch_warn_adv/MapServer',
 	    attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-	    updateFrequency: 30 * 60 * 1000,
+	    cache_defeat: 30 * 60,
 	    cssClass: "ol-semi-transparent",
 	},
 	{
@@ -411,14 +575,14 @@ const otherRandomLayersConfig = {
 		//crossOrigin: crossorigin,
 	    },
 	    service: 'radar_meteo_imagery_nexrad_time',
-	    updateFrequency: 2 * 60 * 1000,
+	    cache_defeat: 2 * 60,
 	},
 	{
 	    // This basemap is depricated in favor of the GEBCO map. It doesn't load reliably.
 	    type: 'XYZ',
 	    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer',
 	    attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-	    updateFrequency: 15 * 60 * 1000,
+	    cache_defeat: 15 * 60,
 	},
 	{
 	    type: "WMS",
@@ -429,8 +593,31 @@ const otherRandomLayersConfig = {
 		crossOrigin: crossorigin,
 	    },
 	    service: 'radar_meteo_imagery_nexrad_time',
-	    updateFrequency: 2 * 60 * 1000,
+	    cache_defeat: 2 * 60,
 	},
+	{
+	    // NEXRAD from NowCOAST - but it's raw base reflectivity and I prefer qcd (quality controlled?)
+	    type: 'WMS',
+	    url: 'https://nowcoast.noaa.gov/geoserver/weather_radar/wms',
+	    cache_defeat: 5 * 60,
+	    params: {
+		'LAYERS': 'base_reflectivity_mosaic',
+		'FORMAT': 'image/png',
+	    },
+	},
+	{
+	    // This SST is too coarse
+            // https://coastwatch.pfeg.noaa.gov/erddap/wms/erdHadISST/index.html
+	    type: 'WMS',
+	    url: 'https://coastwatch.pfeg.noaa.gov/erddap/wms/erdHadISST/request',
+	    params: {
+		'LAYERS': 'erdHadISST:sst',
+		'FORMAT': 'image/png',
+	    },
+	    projection: 'EPSG:4326',
+	    cache_defeat: 6 * 60 * 60,
+	},
+
 	// url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	// attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	// { type: "Stamen", layer:  'watercolor' },
