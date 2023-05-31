a91b8be94656ddb8069abb437d0f24d929df10a5
Lance Berc <lanceb@gmail.com>
2023-05-31 09:11:15 -0700

    Limit fetch back catalog; Add more NOAA models to be fetched

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -6,20 +6,18 @@ if [[ $ipaddr =~ "192.168.2" ]] ; then where=dev ; fi
 date
 /usr/bin/python /home/stfyc/bin/sfcanalysis.py
 date
-/usr/bin/python /home/stfyc/bin/nesdis-fetch.py -since 2 -region CONUS-West
+/usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region CONUS-West-500m -timelimit 4 -since 12h
 date
-/usr/bin/python /home/stfyc/bin/nesdis-fetch.py -since 2 -region GOES-West
+/usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region GOES-West -since 2d
 date
 if [[ $where == "dev" ]]; then
-  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region CONUS-West-500m -timelimit 4
+  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region CONUS-West_GLM_1k -timelimit 4 -since 2d
   date
-  /usr/bin/python /home/stfyc/bin/glm-fetch.py -region CONUS-West_GLM_1k -timelimit 4
+  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region GOES-West_GLM_2k -timelimit 4 -since 2d
   date
-  /usr/bin/python /home/stfyc/bin/glm-fetch.py -region GOES-West_GLM_2k -timelimit 4
+  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region CONUS-East -timelimit 4 -since 1d
   date
-  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region CONUS-East -timelimit 4
-  date
-  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region GOES-East -timelimit 4
+  /usr/bin/python /home/stfyc/bin/nesdis-fetch.py -region GOES-East -timelimit 4 -since 2d
   date
 fi
 /usr/bin/python /home/stfyc/bin/fetch-sfcurrents.py -image current
@@ -29,4 +27,9 @@ fi
 #/usr/bin/python /home/stfyc/bin/fetch-obs.py
 date
 /usr/bin/python /home/stfyc/bin/fetch-tide.py
-#/usr/bin/python /home/stfyc/bin/hrrr-fetch.py -region Eddy
+/usr/bin/python /home/stfyc/bin/nomads-fetch.py -region Karl
+/usr/bin/python /home/stfyc/bin/nomads-fetch.py -region Eddy
+/usr/bin/python /home/stfyc/bin/nomads-fetch.py -region NorCal-NAM
+/usr/bin/python /home/stfyc/bin/nomads-fetch.py -region CA-NAM
+/usr/bin/python /home/stfyc/bin/nomads-fetch.py -region NYBOS
+/usr/bin/python /home/stfyc/bin/nomads-fetch.py -region NYBOS-NAM
